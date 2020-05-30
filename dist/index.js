f164d46c5d7ed31f5658d57da28354ac129addab
afeiship <1290657123@qq.com>
2020-05-30 21:10:57 +0800

    feat: build multi packages

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var index = {
-  hello: 'hello',
-};
-
-module.exports = index;
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,15 @@
-export default {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/index.js',
-    format: 'cjs'
+import glob from 'glob';
+
+const files = glob.sync('src/packages/*.js');
+const configs = files.map(item => {
+  const dist = item.replace('src', 'dist');
+  return {
+    input: item,
+    output: {
+      file: dist,
+      format: 'cjs'
+    }
   }
-};
+})
+
+export default configs;
