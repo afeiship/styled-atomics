8a75ac468bf7016c77e219e4724c5363b934dae4
afeiship <1290657123@qq.com>
2021-05-28 16:32:45 +0800

    refactor: flexbox opt

--- packages/styled-box/src/components/elements/flexbox.ts
+++ packages/styled-box/src/components/elements/flexbox.ts
@@ -19,19 +19,19 @@ export default (options) => {
       ${value === 'center' && 'align-items: center; justify-content: center;'}
 
       /* la\lar\ar\lr */
-      ${(value === 'la' || value === 'lar' || value === 'ar' || value === 'lr') && 'width: 100%; flex-direction: row;'}
+      ${['la', 'lar', 'ar', 'lr'].includes(value) && 'width: 100%; flex-direction: row;'}
 
       /* ta|tab|ab|tb */
-      ${(value === 'ta' || value === 'tab' || value === 'ab' || value === 'tb') && 'height: 100%; flex-direction: column;'}
+      ${['ta', 'tab', 'ab', 'tb'].includes(value) && 'height: 100%; flex-direction: column;'}
 
       /* la|lar|ta|tab */
-      ${(value === 'la' || value === 'lar' || value === 'ta' || value === 'tab') && '& > * { &:nth-child(2) { flex: 1; } }'}
+      ${['la', 'lar', 'ta', 'tab'].includes(value) && '& > * { &:nth-child(2) { flex: 1; } }'}
 
       /* ab|ar */
-      ${(value === 'ar' || value === 'ab') && '& > * { &:nth-child(1) { flex: 1; } }'}
+      ${['ar', 'ab'].includes(value) && '& > * { &:nth-child(1) { flex: 1; } }'}
 
       /* lr|tb */
-      ${(value === 'lr' || value === 'tb') && 'justify-content: space-between;'}
+      ${['lr', 'tb'].includes(value) && 'justify-content: space-between;'}
     `;
 
     options.data = data.concat(values);
