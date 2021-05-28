45a7953499460abc5713b6ccd3458f29690f3445
afeiship <1290657123@qq.com>
2021-05-28 18:18:06 +0800

    feat: filterd props

--- packages/styled-box/src/components/index.tsx
+++ packages/styled-box/src/components/index.tsx
@@ -2,10 +2,12 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import nxCompose from '@jswork/next-compose';
+import filterReactProps from '@jswork/filter-react-props';
 import { Props, PluginEntity } from './types';
 import atomics from './composite';
 
 const CLASS_NAME = 'styled-box';
+const FILTERED_PROPS = ['rel', 'x', 'y'];
 
 export default class StyledBox extends Component<Props> {
   static displayName = CLASS_NAME;
@@ -49,15 +51,16 @@ export default class StyledBox extends Component<Props> {
   };
 
   render() {
-    const { className, nodeName, engine, plugins, rel, x, y, ...props } = this.props;
+    const { className, nodeName, engine, plugins, ...props } = this.props;
     const fn = nxCompose.apply(null, atomics.concat(plugins as Array<any>));
     const defaultEntity: PluginEntity = { props: this.props, data: [] };
     const options = fn(defaultEntity);
     const styles = options.data;
+    const theProps = filterReactProps(props, FILTERED_PROPS);
     const Styled = engine!.styled(nodeName)`
       ${styles.join('')}
     `;
 
-    return <Styled className={classNames(CLASS_NAME, className)} {...props} />;
+    return <Styled className={classNames(CLASS_NAME, className)} {...theProps} />;
   }
 }
