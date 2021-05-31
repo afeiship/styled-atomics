04813c85d41614186ba3a5ac3b549425153ba96d
afeiship <1290657123@qq.com>
2021-05-31 11:43:09 +0800

    feat: baic plugin

--- packages/styled-box/src/components/plugin.ts
+++ packages/styled-box/src/components/plugin.ts
@@ -1,6 +1,5 @@
 import { InlinePlugin, EngineProps } from './types';
 
-
 /**
  * plugin="transform-center:xy"
  * plugin={{ name:'transform-center', value: 'xy' }}
@@ -10,34 +9,36 @@ import { InlinePlugin, EngineProps } from './types';
 export default class {
   private entity;
   private plugins: InlinePlugin[];
+  private current: InlinePlugin;
   private values: string[] = [];
   private data: any[];
   private engine: EngineProps;
+
+  get name() {
+    return 'basic-plugin';
+  }
 
   constructor(inEntity) {
     const { props, data } = inEntity;
     const { plugin, engine } = props;
     this.data = data;
     this.plugins = this.normalize(plugin);
+    this.current = this.plugins.find((plugin) => plugin.name === this.name) || this.plugins[0];
     this.engine = engine;
-    // this.plugins = this.normalize(inPlugin);
-    // this.data = inData;
-  }
-
-  public defaults() {
-    return null;
   }
 
   public pipe() {
-    // tobe implements
+    this.values = this.engine.css``;
   }
 
   public merge() {
-    this.entity.data = this.entity.data.concat(this.values);
+    this.data = this.data.concat(this.values);
   }
 
   public get() {
-    return this.entity.data;
+    this.pipe();
+    this.merge();
+    return this.data;
   }
 
   private normalize(inPlugin: string | InlinePlugin): InlinePlugin[] {
