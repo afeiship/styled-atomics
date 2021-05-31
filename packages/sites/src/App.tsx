4a4bd2392c485deee6ad57f0a50b78ed9a719c92
afeiship <1290657123@qq.com>
2021-05-31 23:51:54 +0800

    chore: sitesupdate

--- packages/sites/src/App.tsx
+++ packages/sites/src/App.tsx
@@ -1,7 +1,13 @@
+import styled, { css } from "styled-components";
+import StyledBox from "@jswork/styled-box";
+
+Object.assign(View.defaultProps, { engine: { styled, css } });
+
 function App() {
   return (
     <div className="App">
       Hello world
+      <StyledBox auto wp={4} p={10} bdw="4" bds="dashed">abc</StyledBox>
     </div>
   );
 }
