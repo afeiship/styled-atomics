b7ed78aed6106970c5926aceb7721820817bad40
afeiship <1290657123@qq.com>
2021-06-02 10:04:52 +0800

    feat: update pkg prj

--- packages/sites/src/App.tsx
+++ packages/sites/src/App.tsx
@@ -1,13 +1,13 @@
 import styled, { css } from "styled-components";
 import StyledBox from "@jswork/styled-box";
 
-Object.assign(View.defaultProps, { engine: { styled, css } });
+Object.assign(StyledBox.defaultProps, { engine: { styled, css } });
 
 function App() {
   return (
     <div className="App">
       Hello world
-      <StyledBox p={20} wp={2} auto>
+      <StyledBox debug p={20} wp={2} auto>
         abc
       </StyledBox>
     </div>
