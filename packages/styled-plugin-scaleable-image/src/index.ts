a3f0a62ac0ea2f8b1b63abb95e2e912cd162b07b
afeiship <1290657123@qq.com>
2021-06-25 22:56:52 +0800

    refactor: scaleble image opt

--- packages/styled-plugin-scaleable-image/src/index.ts
+++ packages/styled-plugin-scaleable-image/src/index.ts
@@ -6,20 +6,22 @@ export default class extends StyledAbstractPlugin {
     return 'scaleable-image';
   }
 
+  get defaults() {
+    return { duration: '0.3s', value: 1.05 };
+  }
+
   public pipe() {
     if (!this.current) return;
     const { name, value, duration } = this.current;
-    const dur = duration || '0.3s';
-    const val = value || 1.05;
     this.values = css`
       ${name === this.name && 'display: block; overflow: hidden;'}
       .is-scaleable {
-        transition: all ease-out ${dur};
+        transition: all ease-out ${duration};
       }
 
       &:hover {
         .is-scaleable {
-          transform: scale(${val}, ${val});
+          transform: scale(${value}, ${value});
         }
       }
     `;
