196c9e14cac769a0a135d0237842f668616a0217
afeiship <1290657123@qq.com>
2021-06-28 13:13:01 +0800

    feat: websites initial

---
+++ packages/websites/docusaurus.config.js
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'StyledBox Website',
+  tagline: 'The atomics css solution based on styled components.',
+  url: 'https://github.com/afeiship/styled-atomics',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'afeiship', // Usually your GitHub org/user name.
+  projectName: 'styled-atomics', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'StyledBox',
+      logo: {
+        alt: 'My Facebook Project Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        // Please keep GitHub link to the right for consistency.
+        {
+          href: 'https://github.com/afeiship/styled-atomics',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    }
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ packages/websites/sidebars.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+
+module.exports = {
+  someSidebar: {
+    Welcome: ['mdx'],
+    Generators: ['doc1', 'doc2', 'doc3'],
+    Shorty: ['mdx'],
+    Plugin: ['mdx'],
+  },
+};
